0cba420b3bde59dfd2b61a071a73ae776546ea45
LetMeBeJim <yaobojing@gmail.com>
2023-05-05 20:45:12 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,12 +36,12 @@ app.post('/generate', async (req, res) => {
 
     const response = await cohere.generate({
       model: "command",
-      prompt: "You need to provide a recipe in the format of dish name, ethnicity, ingredients and steps, each begin with a new line. You have to use " + testData.food + " You have to make a " + testData.cuisine + " dish, and the time it takes have to be " + testData.time + " and the difficulty must be " + testData.difficulty,
-      max_tokens: 50,
+      prompt: "You need to provide a full response, the recipe has to be complete. Your dish title must be short and concise. You can only use a max token of 400, wrap up all necessary information within 400 tokens. You need to provide a recipe in the format of dish name, ethnicity, ingredients and steps, each begin with a new line. You have to use " + testData.food + " You have to make a " + testData.cuisine + " dish, and the time it takes have to be " + testData.time + " and the difficulty must be " + testData.difficulty,
+      max_tokens: 400,
       temperature: 1,
     });
 
-    res.json(response)
+    res.json(response.body.generations[0].text)
 })
 
 app.listen(process.env.PORT || 4000, () => { console.log("Server started")})
