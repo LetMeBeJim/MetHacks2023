41c96cfbd8835d6afbd6a83cd29468ee57eec516
LetMeBeJim <yaobojing@gmail.com>
2023-05-05 20:35:22 -0400

    a

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const app = express();
 const cohere = require("cohere-ai");
 cohere.init("MGAAT2e4klv8XjGbVz9RadJEQ4hro2qts6PX8Dim");
 
-
-
+app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
@@ -19,9 +18,30 @@ app.get('/test', (req, res) => {
     res.json({"not good":"Hey again!"});
 })
 
-app.post('/generate', (req, res) => {
-    const data = req.body;
-    console.log('Received Data:', data);
+app.post('/testpost', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+})
+
+app.post('/generate', async (req, res) => {
+    const data = await req.body;
+    console.log(req.body);
+
+    const testData = {
+      "food" : "beef, scallion, potatos, pepper",
+      "cuisine" : "asian",
+      "time" : "fast",
+      "difficulty" : "easy"
+    }
+
+    const response = await cohere.generate({
+      model: "command",
+      prompt: "You need to provide a recipe in the format of dish name, ethnicity, ingredients and steps, each begin with a new line. You have to use " + testData.food + " You have to make a " + testData.cuisine + " dish, and the time it takes have to be " + testData.time + " and the difficulty must be " + testData.difficulty,
+      max_tokens: 50,
+      temperature: 1,
+    });
+
+    res.json(response)
 })
 
 app.listen(process.env.PORT || 4000, () => { console.log("Server started")})
